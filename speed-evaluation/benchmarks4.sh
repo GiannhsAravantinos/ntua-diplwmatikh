ab278112a755ea01276946f97d62b57725ccf127
GiannhsAravantinos <johnnykef@hotmail.com>
2021-02-12 11:30:25 +0200

    revising first benchmark

---
+++ speed-evaluation/benchmarks4.sh
@@ -0,0 +1,17 @@
+./client2 1 512
+./client2 1 1024
+./client2 1 2048
+./client2 1 4096
+./client2 1 8192
+./client2 1 16384
+./client2 1 32768
+./client2 1 65536
+
+./client2 3 512
+./client2 3 1024
+./client2 3 2048
+./client2 3 4096
+./client2 3 8192
+./client2 3 16384
+./client2 3 32768
+./client2 3 65536
