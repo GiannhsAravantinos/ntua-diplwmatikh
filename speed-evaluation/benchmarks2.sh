80db7ec7851f17037591503489f2184ce4911548
GiannhsAravantinos <johnnykef@hotmail.com>
2021-02-17 12:19:46 +0200

    12d

--- speed-evaluation/benchmarks2.sh
+++ speed-evaluation/benchmarks2.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 #tmemPUt
-./client2 3 512
-./client2 3 1024
-./client2 3 2048
-./client2 3 4096
-./client2 3 8192
-./client2 3 16384
-./client2 3 32768
-./client2 3 65536
+#./client2 3 512
+#./client2 3 1024
+#./client2 3 2048
+#./client2 3 4096
+#./client2 3 8192
+#./client2 3 16384
+#./client2 3 32768
+#./client2 3 65536
 
 #original tmemPut
-#./client2 5 512
-#./client2 5 1024
-#./client2 5 2048
-#./client2 5 4096
-#./client2 5 8192
-#./client2 5 16384
-#./client2 5 32768
-#./client2 5 65536
+./client2 5 512
+./client2 5 1024
+./client2 5 2048
+./client2 5 4096
+./client2 5 8192
+./client2 5 16384
+./client2 5 32768
+./client2 5 65536
