1de1b27baa04aed5870d109e23b453afe09126bc
GiannhsAravantinos <johnnykef@hotmail.com>
2021-02-21 11:25:28 +0200

    2000 reqs

--- speed-evaluation/benchmarks5.sh
+++ speed-evaluation/benchmarks5.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-./client3 9 512 1000
-./client3 9 1024 1000
-./client3 9 2048 1000
-./client3 9 4096 1000
-./client3 9 8192 1000
-./client3 9 16384 1000
-./client3 9 32768 1000
-./client3 9 65536 1000
+./client3 9 512 2000
+./client3 9 1024 2000
+./client3 9 2048 2000
+./client3 9 4096 2000
+./client3 9 8192 2000
+./client3 9 16384 2000
+./client3 9 32768 2000
+./client3 9 65536 2000
