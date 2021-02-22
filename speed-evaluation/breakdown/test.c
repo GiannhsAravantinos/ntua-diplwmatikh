d37d1d2e7e2db7e0e05839ff0a2737df086d5500
GiannhsAravantinos <johnnykef@hotmail.com>
2021-02-22 12:13:02 +0200

    hgfdz

--- speed-evaluation/breakdown/test.c
+++ speed-evaluation/breakdown/test.c
@@ -19,7 +19,7 @@ int main(){
   long int nanosecs;
 
   while(1){
-    clock_gettime(clk_id, &t12);
+    clock_gettime(clk_id, &tp2);
     clock_gettime(clk_id, &tp2);
 
     nanosecs = (tp2.tv_sec - tp1.tv_sec)*NSEC + tp2.tv_nsec-tp1.tv_nsec;
